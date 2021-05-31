a952149185f081dcdb67393cfa2e7f6f85bb7f36
Trishul Nagenalli <tn74@duke.edu>
2021-05-31 15:15:29 -0400

    navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
+fontawesomecss = `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+`
 bootstrapcss = `
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 `
@@ -9,44 +13,35 @@ customcss = `
 `
 
 nav = `
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-dark bg-primary navbar-expand-lg sticky-top">
+  <a class="navbar-brand" href="./"><img src="https://veerashaivasamajana.github.io/webhost/images/logo.png" width="40" height="40" class="d-inline-block align-top" alt=""></a>
+  <a class="nav-link" href="https://www.facebook.com/vsna.allchapters"> <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i> </a>
+  <a class="nav-link" href="https://www.youtube.com/channel/UC5ivDhhj0pVBYBPoZhTfy4A"> <i class="fa fa-youtube-play fa-2x" aria-hidden="true"></i> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="./"> Home </a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Organization
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="elected-bod">Elected BOD</a>
+          <a class="dropdown-item" href="resources">Resources</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0" data-children-count="1">
-      <input type="search" class="form-control mr-sm-2" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 `
 
 
+$('head').append(fontawesomecss);
 $('head').append(bootstrapcss);
 $('head').append(customcss);
 window.onload = function() {
