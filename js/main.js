c3652ccc9851cc16cd299dc539118435ac1962d7
Trishul Nagenalli <tn74@duke.edu>
2021-06-05 13:06:42 -0400

    learn

--- js/main.js
+++ js/main.js
@@ -42,6 +42,15 @@ nav = `
           <a class="dropdown-item" href="events">Events</a>
         </div>
       </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Learn
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="publications">Publications</a>
+          <a class="dropdown-item" href="newsletters">Newsletters</a>
+        </div>
+      </li>
     </ul>
   </div>
 </nav>
