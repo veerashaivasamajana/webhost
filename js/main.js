b9657de3a2ce2f91be98ce92512170268ea7e82c
Trishul Nagenalli <tn74@duke.edu>
2021-06-13 18:45:57 -0400

    Live link

--- js/main.js
+++ js/main.js
@@ -24,6 +24,9 @@ nav = `
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
+        <a class="btn btn-danger" href="live">Live</a>
+      </li>
+      <li class="nav-item">
         <a class="btn btn-light" href="convention">Convention</a>
       </li>
       <li class="nav-item">
