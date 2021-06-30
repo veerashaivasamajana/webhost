84520d50f26da0bfdb3dc37ce0483c9f315065ce
Trishul Nagenalli <tnagenalli@abnormalsecurity.com>
2021-06-30 09:54:39 -0600

    subscribe

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ customcss = `
 nav = `
 <nav class="navbar navbar-dark bg-primary navbar-expand-lg sticky-top">
   <a class="navbar-brand" href="./"><img src="https://veerashaivasamajana.github.io/webhost/images/logo.png" width="40" height="40" class="d-inline-block align-top" alt=""></a>
-  <a class="nav-link" href="https://www.facebook.com/vsna.allchapters"> <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i> </a>
+  <a class="nav-link" href="https://www.facebook.com/vsna.allchapters"> <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i> <p>Subscribe</p> </a>
   <a class="nav-link" href="https://www.youtube.com/channel/UC5ivDhhj0pVBYBPoZhTfy4A"> <i class="fa fa-youtube-play fa-2x" aria-hidden="true"></i> </a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -30,6 +30,7 @@ nav = `
           Convention
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="https://vsna.org/convention">Convention Home</a>
           <a class="dropdown-item" href="https://vsna.org/convention/committees">Committees</a>
           <a class="dropdown-item" href="https://vsna.org/convention/program">Program</a>
           <a class="dropdown-item" href="https://vsna.org/convention/competitions">competitions</a>
